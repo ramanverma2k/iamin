6eceabd25efdf7481f16eb4c346315b10c4ea808
ramanverma2k <ramanverma4183@gmail.com>
2021-12-05 11:47:46 +0530

    homepage: show both categories card and transactions list widgets

--- lib/home/home_page.dart
+++ lib/home/home_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:iamin/home/widgets/payment_categories_card.dart';
+import 'package:iamin/home/widgets/transactions_list.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -12,27 +14,67 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Padding(
-              padding: const EdgeInsets.all(20.0),
-              child: ElevatedButton(
-                onPressed: () {},
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: ElevatedButton(
+                  onPressed: () {},
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      const Text('Card Balance'),
+                      Row(
+                        children: const [
+                          Text('\u{20B9}0\t\t\t >'), // unicode for Rupee symbol
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              const Padding(
+                padding: EdgeInsets.only(bottom: 20.0, left: 20.0, right: 20.0),
+                child: Text(
+                  'Payment Categories',
+                  style: TextStyle(color: Color(0xFF8e9eb6)),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 20.0),
+                child: SizedBox(
+                  height: MediaQuery.of(context).size.height * 0.3,
+                  child: const PaymentCategoriesCard(),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(
+                    bottom: 20.0, left: 20.0, right: 20.0, top: 10.0),
                 child: Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-                    const Text('Card Balance'),
-                    Row(
-                      children: const [
-                        Text('\u{20B9}0\t\t\t >'), // unicode for Rupee symbol
-                      ],
+                    const Text(
+                      'Latest Transactions',
+                      style: TextStyle(color: Color(0xFF8e9eb6)),
                     ),
+                    TextButton(
+                      onPressed: () {},
+                      child: const Text(
+                        'Show All\t\t\t >',
+                        style: TextStyle(color: Color(0xFF8e9eb6)),
+                      ),
+                    )
                   ],
                 ),
               ),
-            ),
-          ],
+              const Padding(
+                padding: EdgeInsets.only(bottom: 20.0),
+                child: TransactionsList(),
+              ),
+            ],
+          ),
         ),
       ),
     );
